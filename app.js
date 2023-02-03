6c790e4c47cd44e14df7c4aeebd2cf109b2a76f7
Hariadhenny <hariadhenny@gmail.com>
2023-02-03 10:02:10 -0300

    calculadoraJS

---
+++ app.js
@@ -0,0 +1,27 @@
+let n1 = document.querySelector("#n1");
+let n2 = document.querySelector("#n2");
+let resultado = document.querySelector("span");
+
+function somar() {
+  resultado.innerHTML = parseInt(n1.value) + parseInt(n2.value);
+  document.getElementById("n1").value = "";
+  document.getElementById("n2").value = "";
+}
+
+function subtrair() {
+  resultado.innerHTML = parseInt(n1.value) - parseInt(n2.value);
+  document.getElementById("n1").value = "";
+  document.getElementById("n2").value = "";
+}
+
+function multiplicar() {
+  resultado.innerHTML = parseInt(n1.value) * parseInt(n2.value);
+  document.getElementById("n1").value = "";
+  document.getElementById("n2").value = "";
+}
+
+function dividir() {
+  resultado.innerHTML = parseInt(n1.value) / parseInt(n2.value);
+  document.getElementById("n1").value = "";
+  document.getElementById("n2").value = "";
+}
